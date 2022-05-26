fb743b6b985b1e9dfe333d75bb3aee71211bf4ce
iplatos <platonoff9168@gmail.com>
2022-05-26 21:21:00 +0300

    9-11

---
+++ src/Component/Profile.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = () =>{
+   return     <div className='content'>
+       <div>
+           <img src='https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg'/>
+       </div>
+       <div> ava + discription</div>
+       <div>my posts
+           <div>new post</div>
+           <div>post1</div>
+           <div>post2</div>
+       </div>
+   </div>;
+}
+
+export default Profile
